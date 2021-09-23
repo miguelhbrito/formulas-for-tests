a00d71af733fe65a9d9ca60c5e32c6dc99ef91e4
miguelhbrito <miguelhbrito@gmail.com>
2021-09-23 17:20:16 -0300

    formulas for integration tests

---
+++ ritchie/login/stg/src/formula/formula.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+runFormula() {
+  echo "send request"
+  
+  curl --location --request POST 'https://dennis.stgdennis.zup.io/login' \
+  --header 'Content-Type: application/json' \
+  --header 'x-org: default' \
+  --data-binary "{ \"username\":\"$USERNAME\", \"password\":\"$PASSWORD\" }"
+
+  echo "done"
+}
---
+++ ritchie/talkTo/me/src/formula/formula.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+runFormula() {
+  echo "send request"
+
+  echo "First variable is: $VAR1"
+  echo "Second variable is: $VAR2"
+  echo "Third variable is: $VAR3"
+  echo "Fourth variable is: $VAR4"
+
+  echo "done"
+}
